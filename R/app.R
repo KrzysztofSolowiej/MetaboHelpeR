738d6d4767300d8bca7bd7a33100a4f56e41f39e
Krzysztof Solowiej <krzysztof.solowiej@umb.edu.pl>
2024-05-29 10:53:47 +0200

    titlePanel

--- R/app.R
+++ R/app.R
@@ -33,7 +33,10 @@ library(preprocessCore)
 
 ui <- fluidPage(
   useShinyjs(),
-  titlePanel("MetaboHelpeR"),
+  titlePanel(title = span("MetaboHelpeR", style = "color: #0052cc; font-size: 55px; font-weight: bold; font-family: 'K2D';")),
+  tags$head(
+    tags$link(rel = "stylesheet", href = "https://fonts.googleapis.com/css2?family=K2D:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800&display=swap")
+  ),
   sidebarLayout(
     sidebarPanel(
       uiOutput('file_input'),
